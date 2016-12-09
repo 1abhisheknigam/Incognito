4b8127d46b6d7e9623d4e662d8d39b8d060813ca
Battulga Myagmarjav <Tulga@Battulgas-MacBook-Pro.local>
2016-12-08 23:32:42 -0500

    last push credit

--- Assets/MENU/Scripts/CreditsController.cs
+++ Assets/MENU/Scripts/CreditsController.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
@@ -45,66 +44,11 @@ public class CreditsController : MonoBehaviour {
         "Skyboxes: Cerberus\n" +
 		"Blood FX Pack: Robin Schmidt\n" +
 		"Sci-Fi Arsenal: Kenneth \"Archanor\" Foldal Moe\n" +
-		"Middle-East Environment: Manufactura K4\n"
-        "Sounds: Adam_N, Apple loops, Avery Alexander, Phil25, pgi, senitiel, UncleSigmund\n" +
-        "Music: Hunter, Incognito by Avery Alexander -https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode (unmodified)" + 
+		"Middle-East Environment: Manufactura K4\n" +
+		"Sounds: Adam_N, Apple loops, InspectorJ, Phil25, pgi, senitiel, UncleSigmund\n" +
+		"Music: \"Hunter\", \"Incognito\" by Avery Alexander -https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode (unmodified)" + 
         "\n\n\n" +
         "COPYRIGHT: TEAM INCOGNITO\n" +
         "End\n";
     }
 }
-=======
-﻿using UnityEngine;
-using System.Collections;
-using UnityEngine.UI;
-
-public class CreditsController : MonoBehaviour {
-    Animator myAnim;
-	// Use this for initialization
-	void Start () {
-        myAnim = GetComponent<Animator>();
-        myAnim.enabled = true;
-        setCreditText();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (GetComponentInParent<Canvas>().enabled)
-        {
-            myAnim.enabled = true;            
-        }
-        else 
-        {
-            myAnim.enabled = false;
-        }
-	}
-
-    void setCreditText()
-    {
-        GetComponent<Text>().text =
-        "-INCOGNITO-\n" +
-        "\nCredits\n" +
-        "AI Programming\nCheng Hann Gan\nJimmy Spearman\n\n" +
-        "Character Design\nAbhishek Nigam\nJimmy Spearman\n\n" +
-        "Gameplay Programming\nCheng Hann Gan\nOdell Mizrahi\nAbhishek Nigam\nJimmy Spearman\n\n" +
-        "Level Design\nCheng Hann Gan\nOdell Mizrahi\nTulga Myagmarjav\n\n" +
-        "Sound Design\nCheng Hann Gan\nJimmy Spearman\n\n" +
-        "UI Design\nTulga Myagmarjav\nAbhishek Nigam\n\n" +
-        "External Resources\n" +
-        "AI: RAIN AI\n" +
-        "Camera: Unity stealth game tutorial\n" +
-        "Character Models/ Animations: Mixamo\n" +
-        "Environment Art: Manufactura K4, ProBuilder\n" +
-        "Font: Pirulen\n" +
-        "Image Effects: Unity standard assets\n" +
-        "Skyboxes: Cerberus\n" +
-		"Blood FX Pack: Robin Schmidt\n" +
-		"Sci-Fi Arsenal: Kenneth \"Archanor\" Foldal Moe\n" +
-        "Sounds: Adam_N, Apple loops, InspectorJ, Phil25, pgi, senitiel, UncleSigmund\n" +
-        "Music: \"Hunter\", \"Incognito\" by Avery Alexander -https://creativecommons.org/licenses/by-nc-sa/3.0/legalcode (unmodified)" + 
-        "\n\n\n" +
-        "COPYRIGHT: TEAM INCOGNITO\n" +
-        "End\n";
-    }
-}
->>>>>>> 7c44f77b077508b70f207d901511ac667b43e098
